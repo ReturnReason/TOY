d582ae59c63d5d87bbcb59a0218ad15b5da768b7
REASON <reasonz@daum.net>
2023-07-06 12:53:17 +0900

    refactor: %를 계산하는 함수 분리

--- scroll/js/main.js
+++ scroll/js/main.js
@@ -1,13 +1,15 @@
 const $title = document.querySelector('.title');
 
+const percent = (scrollNum, totalScrollNum) => {
+  return `${((scrollNum / totalScrollNum) * 100).toFixed(0)}%`;
+};
+
 const calcScroll = () => {
   let scrollNum = 0;
 
   addEventListener('scroll', () => {
     scrollNum = scrollY;
-    $title.innerText = `${((scrollNum / (document.body.scrollHeight - innerHeight)) * 100).toFixed(
-      0
-    )}%`;
+    $title.innerText = percent(scrollNum, document.body.scrollHeight - innerHeight);
   });
 };
 
