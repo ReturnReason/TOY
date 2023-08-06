16e2db427e0e25dadc8e6cbe2ad5b2093c480546
ReturnReason <reasonz@daum.net>
2023-08-06 15:55:34 +0900

    feat: SSG 페이지 추가

---
+++ my-app/src/app/products/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ProductPage({ params }: Props) {
+  console.log(params);
+
+  return <div>{params.slug}ProductPage</div>;
+}
+
+// 다이나믹 라우트 사용하는 페이지에서 어떤 페이지를 미리 만들지 명시
+// 어떤 경로에 대해서 만들고 싶을 지 generateStaticParams 사용
+export function generateStaticParams() {
+  const products = ["pants", "skirt"]; // pants, skirt 경로에 대해서 미리 만들기
+
+  // prams에 들어갈 Props 형태로 객체 만들어주면 됨.
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
