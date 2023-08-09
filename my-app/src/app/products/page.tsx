c6b226a4207dc4d76af22bd98956911eb5e724c2
ReturnReason <reasonz@daum.net>
2023-08-10 05:09:31 +0900

    refactor: 중복되는 코드 map 함수로 변경

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,22 +1,17 @@
 import Link from "next/link";
 
+const products = ["shirt", "pants", "skirt", "shoes"];
+
 export default function Products() {
   return (
     <div>
       <h2>Product</h2>
       <ul>
-        <li>
-          <Link href="/products/shirt">shirt</Link>
-        </li>
-        <li>
-          <Link href="/products/pants">pants</Link>
-        </li>
-        <li>
-          <Link href="/products/skirt">skirt</Link>
-        </li>
-        <li>
-          <Link href="/products/shoes">shoes</Link>
-        </li>
+        {products.map((product, idx) => (
+          <li key={`${product} ${idx}`}>
+            <Link href={`/products/${product}`}>{product}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
