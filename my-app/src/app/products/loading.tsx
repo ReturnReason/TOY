76cfa94511a6162fe37790423ff626b963589564
ReturnReason <reasonz@daum.net>
2023-08-21 05:59:50 +0900

    feat: 로딩 페이지 추가

---
+++ my-app/src/app/products/loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ProductsLoading() {
+  return <div>Loading...</div>;
+}
+
+/**
+ * loading은 해당 경로 레이아웃의 chidren을 서스펜스로 감싸서 동작함.
+ * 리액트의 서스펜스 바운더리 사용
+ */
