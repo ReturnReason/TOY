3eb9398601b2476cc4b8127523676924057f84cd
ReturnReason <reasonz@daum.net>
2023-08-28 05:51:32 +0900

    feat: 임시 remotePatterns 추가

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.namu.wiki",
+        port: "",
+        pathname: "/i/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,5 +1,23 @@
+import Card from "@/components/common/Card";
 import Image from "next/image";
 
+const TEMP_IMG_URL =
+  "https://i.namu.wiki/i/1k5hig4AXdVecBNZx91Uyw8gy5cF62ksBezsDdr3Lj8nqHqsR1T6uxjBnR3i-6hmggnHqvZ1hw-EpahHS3AOcWqhXsXtE8XjhU7Wg5tkbvePDt05i2dRHGsUHBWlddFm7PWcn4UEYMnffgQtw31kGA.webp";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Card
+        title={"게시글 타이틀"}
+        desc={"게시글 내용"}
+        image={{
+          src: TEMP_IMG_URL,
+          alt: "아이유",
+          width: 500,
+          height: 500,
+          placeholder: "empty",
+        }}
+      />
+    </main>
+  );
 }
